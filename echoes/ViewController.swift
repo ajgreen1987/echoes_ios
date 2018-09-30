5114a21be6afbd2c407ed9c9d2fe24ce773b6aa5
AJ Green <ajgreen@andrews-macbook.attlocal.net>
2018-09-30 15:03:35 -0400

    updating the client name within the sample doInvoke()

--- echoes/ViewController.swift
+++ echoes/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 import AWSAuthCore
 import AWSAuthUI
+import AWSCore
+import AWSAPIGateway
+import AWSMobileClient
 
 
 class ViewController: UIViewController {
@@ -43,6 +46,57 @@ class ViewController: UIViewController {
             // Note: The showSignIn() method used below was added by us previously while integrating the sign-in UI.
             self.showSignIn()
         })
+    }
+    
+    func doInvokeAPI() {
+        // change the method name, or path or the query string parameters here as desired
+        let httpMethodName = "POST"
+        // change to any valid path you configured in the API
+        let URLString = "/items"
+        let queryStringParameters = ["key1":"{value1}"]
+        let headerParameters = [
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        ]
+        
+        let httpBody = "{ \n  " +
+            "\"key1\":\"value1\", \n  " +
+            "\"key2\":\"value2\", \n  " +
+        "\"key3\":\"value3\"\n}"
+        
+        // Construct the request object
+        let apiRequest = AWSAPIGatewayRequest(httpMethod: httpMethodName,
+                                              urlString: URLString,
+                                              queryParameters: queryStringParameters,
+                                              headerParameters: headerParameters,
+                                              httpBody: httpBody)
+        
+        // Create a service configuration
+        let serviceConfiguration = AWSServiceConfiguration(region: AWSRegionType.USEast1,
+                                                           credentialsProvider: AWSMobileClient.sharedInstance().getCredentialsProvider())
+        
+        // Initialize the API client using the service configuration
+        ECHOESRESTAPIEchoesaddcClient.registerClient(withConfiguration: serviceConfiguration!, forKey: "CloudLogicAPIKey")
+        
+        // Fetch the Cloud Logic client to be used for invocation
+        let invocationClient = ECHOESRESTAPIEchoesaddcClient.client(forKey: "CloudLogicAPIKey")
+        
+        invocationClient.invoke(apiRequest).continueWith { (task: AWSTask) -> Any? in
+            if let error = task.error {
+                print("Error occurred: \(error)")
+                // Handle error here
+                return nil
+            }
+            
+            // Handle successful result here
+            let result = task.result!
+            let responseString = String(data: result.responseData!, encoding: .utf8)
+            
+            print(responseString)
+            print(result.statusCode)
+            
+            return nil
+        }
     }
     
 }
